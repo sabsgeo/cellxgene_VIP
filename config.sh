f78baa0ad917c5e5a2c041398f5d836df67db18c
Sabu George <sabugeorge.mec@gmail.com>
2023-07-10 18:32:53 +0530

    update version

--- config.sh
+++ config.sh
@@ -38,8 +38,8 @@ cd cellxgene
 #git checkout bdfd9fe0a5462a0c139675fe10356765d2bbd95b # v 0.16.8
 git checkout f48d06fb9043771d7370ee9ac0dc9de8ae6ad888 # v1.1.1
 #sed -i 's|anndata>=0.7.0|anndata>=0.7.4|' 'server/requirements.txt'
-sed -i 's|anndata>=0.7.6|anndata=0.7.6|' 'server/requirements.txt'
-sed -i 's|numba=0.51.2|numba>=0.51.2|' 'server/requirements.txt'
+sed -i 's|anndata>=0.7.6|anndata==0.7.6|' 'server/requirements.txt'
+sed -i 's|numba>=0.51.2|numba==0.51.2|' 'server/requirements.txt'
 #sed -i 's|scanpy==1.4.6|scanpy==1.6.1|' 'server/requirements.txt'
 cd ..
 
